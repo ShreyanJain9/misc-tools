3c352d48b265dd2e8e3eae5bb9a2e97ddc0bfe06
Shreyan Jain <shreyan.jain.9@outlook.com>
2023-03-15 22:39:38 -0700

    addition and multiplication

--- sinatra/calc.rb
+++ sinatra/calc.rb
@@ -3,7 +3,14 @@ require "sinatra"
 get "/" do
   "Num in url pls"
 end
-get "/:num/:num1" do
+get "/x/:num/:num1" do
+  @num = params[:num]
+  @num1 = params[:num1]
+  num2 = @num.to_i * @num1.to_i 
+  num2.to_s
+end
+
+get "/+/:num/:num1" do
   @num = params[:num]
   @num1 = params[:num1]
   num2 = @num.to_i + @num1.to_i 
