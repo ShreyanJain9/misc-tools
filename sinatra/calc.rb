57822b30f32a035707ed006c5b748efe0e5563ee
Shreyan Jain <shreyan.jain.9@outlook.com>
2023-03-15 22:35:55 -0700

    tried more ruby stuff

--- sinatra/calc.rb
+++ sinatra/calc.rb
@@ -3,8 +3,9 @@ require "sinatra"
 get "/" do
   "Num in url pls"
 end
-get "/:num" do
+get "/:num/:num1" do
   @num = params[:num]
-  num2 = @num.to_i + 1 
+  @num1 = params[:num1]
+  num2 = @num.to_i + @num1.to_i 
   num2.to_s
 end
